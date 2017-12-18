67eab597d69940460e073267c6ca27242e0d0162
Derek Miranda <derek.miranda@ff0000.com>
2017-12-18 10:28:52 -0800

    Moved Canvas and Particles out

--- index.js
+++ index.js
@@ -1,38 +1,15 @@
 export { default as Align } from './lib/Align'
-export { default as CanvasBlendMode } from './lib/canvas/CanvasBlendMode'
-export { default as CanvasBlur } from './lib/canvas/CanvasBlur'
-export { default as CanvasCircle } from './lib/canvas/CanvasCircle'
-export { default as CanvasColoring } from './lib/canvas/CanvasColoring'
-export { default as CanvasDrawer } from './lib/canvas/CanvasDrawer'
-export { default as CanvasElement } from './lib/canvas/CanvasElement'
-export { default as CanvasImage } from './lib/canvas/CanvasImage'
-export { default as CanvasLineTo } from './lib/canvas/CanvasLineTo'
-export { default as CanvasPath } from './lib/canvas/CanvasPath'
-export { default as CanvasRect } from './lib/canvas/CanvasRect'
-export { default as CanvasShape } from './lib/canvas/CanvasShape'
-export { default as CanvasSprite } from './lib/canvas/CanvasSprite'
-export { default as CanvasText } from './lib/canvas/CanvasText'
-export { default as CanvasTexture } from './lib/canvas/CanvasTexture'
-export { default as CanvasTiling } from './lib/canvas/CanvasTiling'
-export { default as CanvasTweener } from './lib/canvas/CanvasTweener'
 export { default as Clamp } from './lib/Clamp'
 // export { default as Countdown } from './lib/Countdown'
 export { default as Effects } from './lib/Effects'
 export { default as FullScreen } from './lib/FullScreen'
-export { default as Emitter } from './lib/particles/Emitter'
-export { default as Graphic } from './lib/particles/Graphic'
-export { default as Particle } from './lib/particles/Particle'
-export { default as ParticleModel } from './lib/particles/ParticleModel'
-export { default as ParticlesUtils } from './lib/particles/ParticlesUtils'
 export { default as Ratio } from './lib/Ratio'
 
 // named exports
 import * as Styles from './lib/Styles'
 import * as Markup from './lib/Markup'
-import * as CanvasUtils from './lib/canvas/CanvasUtils'
 
 export {
 	Markup,
 	Styles,
-	CanvasUtils,
 }
