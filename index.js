65ab0ec29bea71e45b2f9bcbe19dce80962a940d
Derek Miranda <derek.miranda@ff0000.com>
2017-12-19 14:33:03 -0800

    Ratio optim

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // export { default as Countdown } from './lib/Countdown'
 export { default as FullScreen } from './lib/FullScreen'
-export { default as Ratio } from './lib/Ratio'
 
 // named exports
+import * as Ratio from './lib/Ratio'
 import * as Effects from './lib/Effects'
 import * as Clamp from './lib/Clamp'
 import * as Align from './lib/Align'
@@ -10,6 +10,7 @@ import * as Styles from './lib/Styles'
 import * as Markup from './lib/Markup'
 
 export {
+	Ratio,
 	Effects,
 	Clamp,
 	Align,
