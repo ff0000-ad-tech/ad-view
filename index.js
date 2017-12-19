68d33adc5e03afc6def1ce856877bc9a536adca1
Derek Miranda <derek.miranda@ff0000.com>
2017-12-19 13:51:37 -0800

    Clamp exports

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-export { default as Clamp } from './lib/Clamp'
 // export { default as Countdown } from './lib/Countdown'
 export { default as Effects } from './lib/Effects'
 export { default as FullScreen } from './lib/FullScreen'
 export { default as Ratio } from './lib/Ratio'
 
 // named exports
+import * as Clamp from './lib/Clamp'
 import * as Align from './lib/Align'
 import * as Styles from './lib/Styles'
 import * as Markup from './lib/Markup'
