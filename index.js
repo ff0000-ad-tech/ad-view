e414a092236de2cd60726ca13c065f37c3ca9e61
Derek Miranda <derek.miranda@ff0000.com>
2017-12-19 14:29:16 -0800

    Effects optim

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 // export { default as Countdown } from './lib/Countdown'
-export { default as Effects } from './lib/Effects'
 export { default as FullScreen } from './lib/FullScreen'
 export { default as Ratio } from './lib/Ratio'
 
 // named exports
+import * as Effects from './lib/Effects'
 import * as Clamp from './lib/Clamp'
 import * as Align from './lib/Align'
 import * as Styles from './lib/Styles'
 import * as Markup from './lib/Markup'
 
 export {
+	Effects,
 	Clamp,
 	Align,
 	Markup,
