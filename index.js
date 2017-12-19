41ee531af8d48e41c3d48336dd0dc7ed9e2c4706
Derek Miranda <derek.miranda@ff0000.com>
2017-12-19 11:54:42 -0800

    Named exports - Align

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-export { default as Align } from './lib/Align'
 export { default as Clamp } from './lib/Clamp'
 // export { default as Countdown } from './lib/Countdown'
 export { default as Effects } from './lib/Effects'
@@ -6,10 +5,12 @@ export { default as FullScreen } from './lib/FullScreen'
 export { default as Ratio } from './lib/Ratio'
 
 // named exports
+import * as Align from './lib/Align'
 import * as Styles from './lib/Styles'
 import * as Markup from './lib/Markup'
 
 export {
+	Align,
 	Markup,
 	Styles,
 }
